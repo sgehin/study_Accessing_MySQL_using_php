5c0565c995b2d21ab7ff20290b8ae9efaea28d4c
sgehin <sfa.gehin@gmail.com>
2017-11-19 15:24:10 +0100

    preventing Hacking Attempts

--- index.php
+++ index.php
@@ -1,27 +1,43 @@
-<!DOCTYPE html>
-<!-- using insert IDs to make relation between 2 tables-->
 
-<?php include 'connection.php'; ?>
+<!-- preventing Hacking Attempts
+
+function mysql_fix_string($conn,$string) = in connection.php
+DB= go 
+tabel = username
+
+-->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+    <script>      
+        function toFormInlog() {
+        document.location ='inlog_form.php';
+        }
+        
+    </script>
     </head>
     <body>
+            <button class="knop" onclick="toFormInlog()">INLOG</button>
+
         <?php
+        require_once 'connection.php';
         $conn=connectDB();
-        $sql = "INSERT INTO cats value (NULL,'Lynx','Stumpy','5')";
-        $result = $conn->query($sql);
-        $insertID = $conn-> insert_id;
         
-        $sql= "INSERT INTO owners value ($insertID,'Ann','Smith')";
-        $result = $conn->query($sql);
+        $user = mysql_fix_string($conn, $_POST['user']);
+        $password = mysql_fix_string($conn, $_POST['pass']);
+        $sql = "SELECT * FROM username WHERE username = '$user' AND password = '$password'";
+    
+        
+        
+        
+        
+        
         ?>
     </body>
     <!-- resultaat:
     
-    ID 3 in tabel cats wordt ook in tabel owners 3 in kolom ID
-    
+   
     -->
     
 </html>
